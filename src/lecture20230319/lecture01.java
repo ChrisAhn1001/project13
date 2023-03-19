680415d0def5d3efd89ce76624e168adddd9a429
ChrisAhn1001 <chrisjahn00@gmail.com>
2023-03-19 14:18:31 +0900

    업로드

---
+++ src/lecture20230319/lecture01.java
@@ -0,0 +1,21 @@
+package lecture20230319;
+
+public class lecture01 {
+    public static void main(String[] args) {
+        // 기본형, primitive type
+        //정수형
+        byte number1 = 1;
+        short number2 = 2;
+        int number3 = 3;
+        long number4 = 4L;
+        //실수형
+        float fNumber =1l.0f
+        double dNumber = 22.0;
+        //문자형
+        char character = 'A';
+        // boolean형
+        boolean flag = true;
+        // 창조형, reference tpye
+        String str = "HEllo world"
+    }
+}
