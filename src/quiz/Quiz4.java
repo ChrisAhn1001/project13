9efc8f9b936234d464b909c2a69aaf83c82c7e41
ChrisAhn1001 <chrisjahn00@gmail.com>
2023-04-16 14:48:01 +0900

    정리

---
+++ src/quiz/Quiz4.java
@@ -0,0 +1,11 @@
+package quiz;
+
+public class Quiz4 {
+    public static void main(String[] args) {
+        String name = "홍길동";
+        int studentNumber = 20181221104;
+        double tail = 1.78;
+        boolean isMale = "true";
+
+    }
+}
